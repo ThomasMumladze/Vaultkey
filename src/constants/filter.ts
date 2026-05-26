0aed333587154894640255485bb28bb5abfe619f
ThomasMumladze <mumladzea4@gmail.com>
2026-05-26 17:07:41 +0400

    add filter constant

---
+++ src/constants/filter.ts
@@ -0,0 +1,33 @@
+export const categories = ["Gift Card", "Game Key", "Random Key", "Mystery key", "Account", "windows", "console"];
+
+export const platform = [
+    "steam",
+    "epic games",
+    "playstation 5",
+    "playstation 4",
+    "xbox series S|X",
+    "xbox one",
+    "nintendo",
+    "ubisoft",
+    "ms store",
+];
+
+export const genre = [
+    "fps",
+    "action",
+    "rpg",
+    "vr games",
+    "strategy game",
+    "racing",
+    "open world",
+    "horror",
+    "sport",
+    "survival",
+    "simulator",
+    "co-op",
+    "fighting",
+    "hack & slash",
+    "puzzle",
+];
+
+export const rating = ["5 star - ⭐⭐⭐⭐⭐", "4 star - ⭐⭐⭐⭐", "3 star - ⭐⭐⭐", "2 star - ⭐⭐"];
